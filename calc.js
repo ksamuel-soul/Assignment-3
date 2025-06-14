be40d2fa3f875b24b97a3c33bacb1322c8adf08d
ksamuel-soul <ksamuel11112@gmail.com>
2025-06-15 01:27:09 +0530

    Third_Commit

--- calc.js
+++ calc.js
@@ -2,27 +2,30 @@ function add()
 {
     num1 = parseFloat(document.getElementById('num1').value);
     num2 = parseFloat(document.getElementById('num2').value);
-    sum = num1 + num2
-    document.getElementById('res').innerHTML = `<p style="color: red; font-size: 20px;">${sum}</p>`;
+    ans = num1 + num2
+    document.getElementById('res').innerHTML = `<p style="color: red; font-size: 20px;">${ans}</p>`;
 }
 
 function sub()
 {
     num1 = parseFloat(document.getElementById('num1').value);
     num2 = parseFloat(document.getElementById('num2').value);
-    document.getElementById('res').innerHTML = num1-num2;
+    ans = num1-num2;
+    document.getElementById('res').innerHTML = `<p style="color: red; font-size: 20px;">${ans}</p>`;
 }
 
 function mult()
 {
     num1 = parseFloat(document.getElementById('num1').value);
     num2 = parseFloat(document.getElementById('num2').value);
-    document.getElementById('res').innerHTML = num1 * num2;
+    ans = num1 * num2;
+    document.getElementById('res').innerHTML = `<p style="color: red; font-size: 20px;">${ans}</p>`;
 }
 
 function div()
 {
     num1 = parseFloat(document.getElementById('num1').value);
     num2 = parseFloat(document.getElementById('num2').value);
-    document.getElementById('res').innerHTML = num1 / num2;
+    ans = num1 / num2;
+    document.getElementById('res').innerHTML = `<p style="color: red; font-size: 20px;">${ans}</p>`;
 }
